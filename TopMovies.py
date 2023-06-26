3d106d5a60ae09d726087e745083ee2462b86a4a
DataWeeb <127965809+DataWeeb@users.noreply.github.com>
2023-06-26 11:43:57 +0530

    TopMovies.py

--- TopMovies.py
+++ TopMovies.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import string
 
 # Create a SparkSession with Hive support
 spark = SparkSession.builder.appName("sparkHiveBase").enableHiveSupport().getOrCreate()
@@ -15,7 +16,7 @@ ratings = "rating"
 
 TopMovies="topmovies"
 
-# Hive query to retrieve the data
+# Hive query to retrieve the data of most watched high rated movies
 df = spark.sql(f""" with cte as 
               (SELECT user_id,movie_id,avg(ratings) as ratings FROM {db}.{ratings}
               GROUP BY user_id, movie_id),
@@ -37,8 +38,44 @@ df = spark.sql(f""" with cte as
 
 """)
 
-
 df.write.mode("overwrite").saveAsTable(f"{db}.{TopMovies}")
+
+
+####partition by alphabets
+
+first_letter=[letter for letter in string.ascii_uppercase]
+
+for i in first_letter:
+    df=spark.sql(f"""select * from movie where substring(moviename,1,1)='{i}' order by movie_id""")
+    partition_path = "/user/movie"
+
+    df.write.mode("overwrite").csv(partition_path)
+
+#it will create folders for every alphabet from A-Z
+
+####partition by genre
+
+
+from pyspark.sql.functions import col,split,explode
+
+
+df=movie.withColumn("genre_list",split(col("genre"),"\\|")).drop("genre")
+
+df2=df.withColumn("genre",explode(df["genre_list"]))
+
+
+df3= df2.select("movie_id", "moviename", "genre")
+
+partition_path = "/user/movie"
+
+df3.write.mode("overwrite").csv(partition_path)
+
+#note# it will create folders according to genres
+
+
+
+
+
 
 
 
